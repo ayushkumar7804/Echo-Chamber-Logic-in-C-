54a5e28fa6274ad096dfbf6612dbfe04056cd0b6
KassArc <ayushkumaryadav2029@gmail.com>
2025-08-06 21:39:38 +0530

    Create EvenNumber.cpp

---
+++ EvenNumber.cpp
@@ -0,0 +1,19 @@
+// Ayush Yadav
+// 24070123028
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    for(int i=1; i<11; i++)
+    {if(i%2==0)
+    {cout<<i<<endl;}}
+    return 0;
+}
+
+// OUTPUT
+// 2
+// 4
+// 6
+// 8
+// 10
