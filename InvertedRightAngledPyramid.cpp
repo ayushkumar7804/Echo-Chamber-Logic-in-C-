94bbce4649cede3800f90558cf000443514a2ba9
KassArc <ayushkumaryadav2029@gmail.com>
2025-08-06 21:46:40 +0530

    Create InvertedRightAngledPyramid.cpp

---
+++ InvertedRightAngledPyramid.cpp
@@ -0,0 +1,28 @@
+// Ayush Yadav
+// 24070123028
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of rows: ";
+    cin >> n;
+
+    for (int i = n; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+// OUTPUT
+// Enter number of rows: 5
+// * * * * * 
+// * * * * 
+// * * *
+// * *
+// *
