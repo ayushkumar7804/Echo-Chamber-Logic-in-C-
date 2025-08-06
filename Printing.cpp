2d7e1f5443c41ebdcb2513e9101e46b37c0ebe73
KassArc <ayushkumaryadav2029@gmail.com>
2025-08-06 21:52:34 +0530

    Create Printing.cpp

---
+++ Printing.cpp
@@ -0,0 +1,18 @@
+// Ayush Yadav
+// 24070123028
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    for(int i=0;i<5;i++)
+    {cout<<"Aditya Jadhav"<<endl;}
+    return 0;
+}
+
+// OUTPUT
+/*Ayush Yadav
+  Ayush Yadav
+  Ayush Yadav
+  Ayush Yadav
+  Ayush Yadav*/
